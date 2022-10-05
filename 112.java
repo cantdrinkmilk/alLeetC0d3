aeead03493ff09e72ac84718e6276f4b3d98ba33
cantdrinkmilk <53961561+cantdrinkmilk@users.noreply.github.com>
2022-10-04 17:15:22 -0700

    112 set up
    First commit to git

---
+++ 112.java
@@ -0,0 +1,29 @@
+package alLeetC0d3;
+import java.util.*;
+
+class Solution{
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val){this.val = val;}
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static void main(String[] args){
+        
+    }
+
+    
+
+    public boolean hasPathSum(TreeNode root, int taargetSum){
+
+            
+        return true;
+    }
+}
+
